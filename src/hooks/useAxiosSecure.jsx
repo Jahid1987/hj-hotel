1ab304d34cda231bcf0ad0b50d37815fdc4eb949
Jahid Anwer <jahid.englaa@gmail.com>
2024-05-14 12:42:17 +0600

    interceptor omited for some fetching issues

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { useEffect } from "react";
-import useAuth from "./useAuth";
-import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import useAuth from "./useAuth";
+// import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "https://hj-hotel.vercel.app",
@@ -9,27 +9,27 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
-  const { signOutUser } = useAuth();
-  const navigate = useNavigate();
+  // const { signOutUser } = useAuth();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    axiosSecure.interceptors.response.use(
-      (res) => {
-        return res;
-      },
-      (error) => {
-        console.log("error tracked in interceptor", error);
-        if (error.response.status === 401 || error.response.status === 403) {
-          console.log("Logout the user");
-          signOutUser()
-            .then(() => {
-              navigate("/login");
-            })
-            .catch((err) => console.log(err));
-        }
-      }
-    );
-  });
+  // useEffect(() => {
+  //   axiosSecure.interceptors.response.use(
+  //     (res) => {
+  //       return res;
+  //     },
+  //     (error) => {
+  //       console.log("error tracked in interceptor", error);
+  //       if (error.response.status === 401 || error.response.status === 403) {
+  //         console.log("Logout the user");
+  //         signOutUser()
+  //           .then(() => {
+  //             navigate("/login");
+  //           })
+  //           .catch((err) => console.log(err));
+  //       }
+  //     }
+  //   );
+  // });
 
   return axiosSecure;
 };
